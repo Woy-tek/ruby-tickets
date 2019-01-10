738daf36653b735123e5ac9b58443f67e05daa28
Woy-tek <woy1157@gmail.com>
2019-01-10 16:12:12 +0100

    First lab

---
+++ app/models/ticket.rb
@@ -0,0 +1,7 @@
+class Ticket < ApplicationRecord
+    validates :name, :presence => true, :length => { :minimum => 3 }
+    validates :email_address, :presence => true, :length => { :minimum => 3 }
+    validates :seat_id_seq, :presence => true
+    validates :address, :presence => true, :length => { :minimum => 6 }
+    validates :price, :presence => true
+end
