9afbc5d29f4f09c6d7f4effbe0a04690bd20d9b4
Woy-tek <woy1157@gmail.com>
2019-01-21 21:18:25 +0100

    Lab3 14 steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'events/index'
   get 'events/new'
   # get 'events/create'
@@ -7,6 +8,6 @@ Rails.application.routes.draw do
   get 'events/:id' => 'events#show'
   resources :tickets
   resources :events, :only => [:index, :new, :create, :show]
-  # root :to => "tickets#index"
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
