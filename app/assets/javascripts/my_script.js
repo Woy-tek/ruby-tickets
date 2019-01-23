c102ce2c46889cd4f3175a17225a83fb8bc1673b
Woy-tek <woy1157@gmail.com>
2019-01-23 11:43:36 +0100

    Add javascript component

--- app/assets/javascripts/my_script.js
+++ app/assets/javascripts/my_script.js
@@ -1,4 +1,17 @@
 
-var fun = function(){
-    alert('AAA');
+var changeBackgroundColor = function(){
+    document.body.style.backgroundColor = getRandomColor();
 }
+
+var resetBackgroundColor = function(){
+    document.body.style.backgroundColor = "white";
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
